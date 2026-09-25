10ea24bcf6c8cfd795d269b98c51adcd934454d1
24120061-dev <24120061@student.bdu.edu.vn>
2026-09-25 09:41:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ CSV_FILE = "history.csv"
 menu = {
     "Đồ ăn": {
         "Pizza Hải Sản": 150000,"Pizza cá": 500000,
-        "Mì Ý Bò Bằm": 95000,
+        "Mì Ý Bò Bằm": 95000,"Gà chiên mắm tỏi":200000,
         "Burger Gà": 35000,
         "Salad Trộn": 50000,
         "Bít tết Bò Mỹ": 250000,
